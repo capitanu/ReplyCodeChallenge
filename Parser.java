43342d84432d2d0179cf8f671d42c4265edfa73d
Oxbrew <13368707+Oxbrew@users.noreply.github.com>
2020-03-12 17:52:39 +0100

    jj

--- Parser.java
+++ Parser.java
@@ -41,7 +41,43 @@ public class Parser {
             }
             newDev.skills(skills);
             devList[i] = newDev;
-        }
+        }import java.io.*;
+import java.util.*;
+
+
+public class Parser {
+	public static void main(String[] args) {
+		int width;
+		int height;
+
+		Scanner in = new Scanner(System.in);
+		width = Integer.parseInt(in.next());
+		height = Integer.parseInt(in.next());
+
+		char[][] map = new char[height][width];
+
+		for (int i = 0; i < height ; i++ ) {
+			for (int j = 0; j < width; j++ ) {
+				map[i][j] = in.next();
+			}
+		}
+
+		for (int i = 0; i < height ; i++ ) {
+			for (int j = 0; j < width ; j++ ) {
+				System.out.print(map[i][j]);
+			}
+		}
+
+
+
+
+
+		//while(in.hasNext()){
+			//System.out.print(in.next());
+			//}
+	}
+}
+
 
 
 
