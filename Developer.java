1a28a41d0d4d62e1cb6eebaa8880d884c9087a7e
Calin Capitanu <calincapitanu@gmail.com>
2020-03-12 16:48:50 +0100

    asfa

--- Developer.java
+++ Developer.java
@@ -1,4 +1,10 @@
 class Developer {
     int bonus;
     String company;
+    int nrOfSkills;
+    String[] skills;
+    public Developer(String company, int company, String[] skills){
+	
+    }
+    
 }
---
+++ PM.java
@@ -0,0 +1,8 @@
+class PM {
+    String company;
+    int bonus;
+    public PM(String company, int bonus){
+	this.company = company;
+	this.bonus = bonus;
+    }
+}
